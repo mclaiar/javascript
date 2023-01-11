d7b98149c57b2e66d13be9a8c63609fd369a7a63
mclaiar <41599509+mclaiar@users.noreply.github.com>
2023-01-11 15:15:06 +0000

    init

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,72 @@
+/*let js = 'amazing';
+//if (js === 'amazing') alert('Javascript is Fun!');
+console.log(40 + 8 + 23 - 10);
+
+console.log("Aaron"); //Aaron is a 'value'
+console.log(23);
+
+let firstName = "Timmy"; //Assign 'value' in variable
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//assignment
+let country = "USA";
+let continent = "N. America";
+let population = 331.9;
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Aaron');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// let y = null;
+// console.log(y);
+// console.log(typeof null);
+
+// const ageAaron = 2 ** 3;
+// console.log(ageAaron);
+
+// function calcBMI(mass, height) {
+//   return mass / height ** 2;
+// }
+
+// console.log('Test Data 1');
+// let markMass = 78;
+// let markHeight = 1.69;
+// let markBMI = calcBMI(markMass, markHeight);
+// let johnMass = 92;
+// let johnHeight = 1.95;
+// let johnBMI = calcBMI(johnMass, johnHeight);
+// console.log(`Mark's BMI: ${markBMI}`);
+// console.log(`John's BMI: ${johnBMI}`);
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(`Does Mark Have Higher BMI? ${markHigherBMI}`);
+
+// console.log('Test Data 2');
+// markMass = 95;
+// markHeight = 1.88;
+// markBMI = calcBMI(markMass, markHeight);
+// johnMass = 85;
+// johnHeight = 1.76;
+// johnBMI = calcBMI(johnMass, johnHeight);
+// console.log(`Mark's BMI: ${markBMI}`);
+// console.log(`John's BMI: ${johnBMI}`);
+// markHigherBMI = markBMI > johnBMI;
+// console.log(`Does Mark Have Higher BMI? ${markHigherBMI}`);
+const age = 18;
+const drink = age >= 21 ? 'wine 🍷' : 'water 💧';
+console.log(`I like to drink ${drink}!`);
